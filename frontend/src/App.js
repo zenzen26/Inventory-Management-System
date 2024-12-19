143533503493135f4026f8fc3564cc6d52af6f4d
Zenzen <thamzien@gmail.com>
2024-12-19 14:36:04 +1100

    update warranty

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginPage from './components/LoginPage';
 import InventoryPage from './components/InventoryPage';
 import InventoryDetailsPage from './components/InventoryDetailsPage';
+import WarrantyPage from './components/WarrantyPage';
 import SuppliersPage from './components/SuppliersPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -14,6 +15,7 @@ function App() {
                 <Route path="/" element={<LoginPage />} />
                 <Route path="/inventories" element={<InventoryPage />} />
                 <Route path="/inventory_details" element={<InventoryDetailsPage />} />
+                <Route path="waranty" element={<WarrantyPage/>}/>
                 <Route path="suppliers" element={<SuppliersPage/>}/>
             </Routes>
         </Router>
